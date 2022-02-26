124634fbce8986b630084a458e53ba4c737e92df
LEANDRO COSTA FERNANDES <83041805+leandrocf79@users.noreply.github.com>
2022-02-25 23:24:37 -0300

    Add files via upload

---
+++ Formataçao/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace formatar
+{
+    class formatacao
+    {
+        static void Main()
+        {
+            double x = 20.232;
+            Console.WriteLine(x);
+            Console.WriteLine(x.ToString("F1"));
+            Console.WriteLine(x.ToString("C"));
+            Console.WriteLine(x.ToString("P"));
+        }
+    }
+}
---
+++ Variaveis/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace OlaMundo
+{
+    class variaveis
+    {
+
+        static void Main()
+        {
+            int x = 4;
+            double y = 3.3;
+            const double frequencia = 60;
+            double area = x * y;
+            Console.WriteLine("\tÁrea do quadrado é: " + area);
+            Console.WriteLine("\tFrequancia: " + frequencia);
+        }
+    }
+ }
