e4d30b5c90298d6818b7344f98dc2afc3fabef26
LEANDRO COSTA FERNANDES <83041805+leandrocf79@users.noreply.github.com>
2022-03-05 13:05:29 -0300

    Add files via upload

---
+++ Fundamentos/Comentarios.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Fundamentos
+{
+    internal class Comentarios
+    {
+        public static void Executar()
+            //evitar comentários desnecessários
+        {
+            Console.WriteLine("Código limpo é sempre melhor");
+            Console.WriteLine("O C# tem xml coments: ///");
+            /// XML COMENTS para gerar tag
+            /// Pode fazer referências para outras partes da documentação
+            /// Precione Ctrl+espaço
+            ///
+            /*Existem também os
+             * comentários óbvios, quando o nome de uma 
+             * varioável já diz todo o necessário para o entendimento dela.
+             */
+            //Pode usar atalho padrão: Selecionar e Ctrl+K+c e Ctrl+k+u
+        }
+    }
+}
---
+++ Fundamentos/PrimeiroProgramaClass1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Fundamentos
+{
+    internal class ProgramaClass1
+    {
+        public static void Executar() // em vez de Main por Executar
+        {
+            System.Console.WriteLine("Primeiro");/*Acima já está "using System", 
+                                                  * portanto não é preciso por no 
+                                                  * código repetidas vezes "System"*/
+            Console.WriteLine("Programa");
+            Console.WriteLine("Terminou!");
+        }
+    }
+}
