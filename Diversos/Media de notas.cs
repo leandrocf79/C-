d63ca65b9361d2e3da2af71d93f14aea79010a24
LEANDRO COSTA FERNANDES <83041805+leandrocf79@users.noreply.github.com>
2022-03-05 12:08:56 -0300

    Add files via upload

---
+++ Diversos/Media de notas.cs
@@ -0,0 +1,31 @@
+﻿namespace CalcularMedias { 
+class MediaNota
+{
+
+    static void Main()
+    {
+        float media;
+        Console.WriteLine("Olá, bem vindo ao programa de cálculo de média!");
+        Console.WriteLine("Irei auxiliar você a calcular a média do seu aluno!");
+        Console.WriteLine("Digite a primeira nota: ");
+        
+
+        float.TryParse(Console.ReadLine(), out float nota1);
+        Console.WriteLine("Digite a segunda nota: ");
+        float.TryParse(Console.ReadLine(), out float nota2);
+        Console.WriteLine("Digite a terceira nota: ");
+        float.TryParse(Console.ReadLine(), out float nota3);
+        
+        media = (nota1 + nota2 + nota3) / 3;
+        Console.WriteLine("A média do aluno é: " + media);
+        if (media < 7)
+        {
+            Console.WriteLine("\n\tAluno está em recuperação");
+        }
+        else
+        {
+            Console.WriteLine("\n\tAluno aprovado");
+        }
+    }
+}
+}
