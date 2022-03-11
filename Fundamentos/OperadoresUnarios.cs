f2db16804ffd9dcd4ebc490b8eb676ea450f9d23
LEANDRO COSTA FERNANDES <83041805+leandrocf79@users.noreply.github.com>
2022-03-11 12:50:11 -0300

    Add files via upload

---
+++ Fundamentos/OperadoresUnarios.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos
+{
+    class OperadoresUnarios
+    {
+        public static void Executar() {
+            var valorNegativo = -5;
+            var numero1 = 2;
+            var numero2 = 3;
+            var booleano = true;
+            Console.WriteLine("valorNegativo = -5; booleano = true; numero1 = 2; numero2 = 3;  \n******************");
+
+            Console.WriteLine("-valorNegativo: ");
+            Console.WriteLine(-valorNegativo);
+            Console.WriteLine("!booleano: ");
+            Console.WriteLine(!booleano);
+
+            numero1++;
+            Console.WriteLine("numero1++ (incremento): ");
+            Console.WriteLine(numero1);
+
+            --numero1;
+            Console.WriteLine("--numero1 (decremento): ");
+            Console.WriteLine(numero1);
+
+            Console.WriteLine("\n(numero1++ == --numero2):");
+            Console.WriteLine(numero1++ == --numero2);/* para BOAS PRÁTICAS evitar esse tipo de
+                                                       * algorítmo, misturando muitas coisas.*/
+            Console.WriteLine($"{numero1} {numero2}");
+        }
+    }
+}
