6f9bb85872a43b069eb3805029a28fd16eec89d6
LEANDRO COSTA FERNANDES <83041805+leandrocf79@users.noreply.github.com>
2022-03-23 18:34:14 -0300

    Add files via upload

--- EstruturaDeControle/EstruturaIf.cs
+++ EstruturaDeControle/EstruturaIf.cs
@@ -21,9 +21,12 @@ namespace CursoCSharp.EstruturasDeControle
             //    bomComportamento = true;
 
             // bomComportamento = entrada == "S" || entrada == "s";
-            bomComportamento = entrada.ToLower() == "s";
+            bomComportamento = entrada.ToLower() == "s"; /*Essa estrutura substitui as outras
+                                                          *3 acima, já cria uma estrutura de valor
+                                                          *lógico e passa o dígito para minusculo*/
 
-            if (nota >= 9.0 && bomComportamento) {
+            if (nota >= 9.0 && bomComportamento)/*nunca por ; aqui*/
+            {
                 Console.WriteLine("Quadro de honra!");
             }
         }
