bbec2f5c1822d6a4066b006fd52c8d6f8b5c65c1
LEANDRO COSTA FERNANDES <83041805+leandrocf79@users.noreply.github.com>
2022-03-31 17:15:57 -0300

    Add files via upload

--- ClassesEMetodos/Membros.cs
+++ ClassesEMetodos/Membros.cs
@@ -1,13 +1,19 @@
 ﻿using System;
+//using System.Collections.Generic;      *não precisa declarar por já estar dentro 
+//using System.Text;                      da mesma classe Pessoa*
 
 namespace CursoCSharp.ClassesEMetodos
-{
-    class Membros
+{ //Esta classe esta vinculada com a classe Pessoa
+    class Membros /*esta aula mostra a possibilidade de criar varios arquivos dentro
+                   * do mesmo namespace, sem ter que abrir novos arquivos, é apenas 
+                   * uma questão de organização.*/
     {
         public static void Executar() {
-            Pessoa sicrano = new Pessoa();
-            sicrano.Nome = "Renato";
-            sicrano.Idade = 21;
+
+            //clsse              construtor     diferenças entre as palavras Pessoas
+                        Pessoa sicrano = new Pessoa();//Segure Ctrl e clicar sobre pessoa
+            sicrano.Nome = "Leandro";
+            sicrano.Idade = 42;
 
             // Console.WriteLine($"{sicrano.Nome} tem {sicrano.Idade} anos.");
 
@@ -16,8 +22,8 @@ namespace CursoCSharp.ClassesEMetodos
             sicrano.ApresentarNoConsole();
 
             var fulano = new Pessoa();
-            fulano.Nome = "Beto";
-            fulano.Idade = 21;
+            fulano.Nome = "Jaspion";
+            fulano.Idade = 57;
 
             var apresentacaoDoFulano = fulano.Apresentar();
             Console.WriteLine(apresentacaoDoFulano.Length);
---
+++ ClassesEMetodos/Pessoa.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.ClassesEMetodos 
+{ //Esta classe esta vinculada com a classe membros
+    class Pessoa  //tipo
+
+    {    //2 atributos
+        public string Nome; //string tem valor padrão NULL
+        public int Idade; // int tem valor padrão 0. Segure Ctrl e 2 clics do mouse sobre int
+
+        //abaixo 2 comportamentos
+        public string Apresentar() {
+            return string.Format(
+                $"Olá! Me chamo {Nome} e tenho {Idade} anos.");
+        }
+
+        public void ApresentarNoConsole() {
+            Console.WriteLine(Apresentar());
+        }
+
+        public void Zerar() {
+            Nome = "";
+            Idade = 0;
+        }
+    }
+}
