949b5e1b16613b28dae88e3ffafe4e9a9c5c8a88
LEANDRO COSTA FERNANDES <83041805+leandrocf79@users.noreply.github.com>
2022-04-02 16:27:45 -0300

    Add files via upload

---
+++ ClassesEMetodos/MetodosComRetorno.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.ClassesEMetodos
+
+{
+    class CalculadoraComum {
+
+        public int Somar(int a, int b) {
+            return a + b;
+        }
+        
+        public int Somar(int a, int b, int c) { /// Neste caso é uma repetição, mesma 
+                                                ///assinatura (Somar), mas não 
+                                                ///vai dar erro por ter um item a mais, "c".
+                                                ///o C# vai entender pela quantidade de dados
+                                                ///digitado pelo usuário.
+            return a + b + c;
+        }
+
+
+        public int Subtrair(int a, int b) {
+            return a - b;
+        }
+
+        public int Multiplicar(int a, int b) {
+            return a * b;
+        }
+    }
+
+    class CalculadoraCadeia {
+        int memoria;
+
+        public CalculadoraCadeia Somar(int a) {
+            memoria += a;
+            return this;
+        }
+
+        public CalculadoraCadeia Multiplicar(int a) {
+            memoria *= a;
+            return this;
+        }
+
+        public CalculadoraCadeia Limpar() {
+            memoria = 0;
+            return this;
+        }
+
+        public CalculadoraCadeia Imprimir() {
+            Console.WriteLine(memoria);
+            return this;
+        }
+
+        public int Resultado() {
+            return memoria;
+        }
+    }
+
+    class MetodosComRetorno
+    {
+        public static void Executar() {
+            var calculadoraComum = new CalculadoraComum();
+            var resultado = calculadoraComum.Somar(3, 77);
+            
+
+            Console.WriteLine(resultado);
+          
+            Console.WriteLine(calculadoraComum.Subtrair(2, 7));
+            Console.WriteLine(calculadoraComum.Multiplicar(4, 4));
+
+            Console.WriteLine("calculadoraCadeia:");
+            var calculadoraCadeia = new CalculadoraCadeia();
+            calculadoraCadeia.Somar(3).Multiplicar(3).Imprimir().Limpar().Imprimir();
+            Console.WriteLine("calculadoraCadeia, resultado:");
+            resultado = calculadoraCadeia.Somar(3).Multiplicar(2).Resultado();
+            Console.WriteLine(resultado);
+        }
+    }
+}
---
+++ ClassesEMetodos/MetodosEstaticos.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.ClassesEMetodos
+{
+    public class CalculadoraEstatica
+    {
+        ///Esse método é de CLASSE ou ESTÁTICO!
+        
+        // static diz que pode acessar diretamente sem criar instância
+        //não pertence mais ao objeto, mas sim à classe 
+        //           pode ser compartilhado!!
+        public static int Multiplicar(int a, int b)
+        {
+            return a * b;
+        }
+
+        // Método de instância!!!
+        public int Somar(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
+    class MetodosEstaticos
+    {
+        public static void Executar()
+        {
+            var resultado = CalculadoraEstatica.Multiplicar(2, 3);
+            Console.WriteLine("Resultado, multiplicar: {0}", resultado);
+
+            CalculadoraEstatica calc = new CalculadoraEstatica();
+            Console.WriteLine("Soma: " + calc.Somar(2, 7));
+            //Console.WriteLine(calc.Multiplicar(2, 7));     // desse modo vai dar erro
+                                                             //tem que acessar pela classe
+        }
+    }
+}
